620b7a227873661cee908d9f03b7b023c4959c24
davalcato <davalcato@yahoo.com>
2021-02-05 20:48:06 -0800

    added the SignInWithAppleDelegates onto the LaunchScreenView page

--- Clothesy/Views/LaunchScreenView.swift
+++ Clothesy/Views/LaunchScreenView.swift
@@ -11,16 +11,55 @@ import AuthenticationServices
 
 struct LaunchScreenView: View {
     
+    @EnvironmentObject var signInWithAppleManager: SignInWithAppleManager
+    
+    @State private var signInWithappleDelegates: SignInWithAppleDelegates! = nil
+    
     @State private var isAlertPresented = false
     @State private var errDescription = ""
 
     var body: some View {
         VStack {
             Text("Loading...")
+        }.onAppear {
+            self.signInWithAppleManager.checkUserAuth { (authState) in
+                // Switch for the AuthState
+                switch authState {
+                case .undefined:
+                    print("Auth State: .undefined")
+                    self.performExistingAccountSetupFlows()
+                case .signedOut:
+                    print("Auth State: .signedOut")
+                case .signedIn:
+                    print("Auth State: .signedIn")
+                }
+            }
             
+        }.alert(isPresented: $isAlertPresented) {
+            Alert(title: Text("Error"), message:
+                  Text(errDescription), dismissButton:
+                    .default(Text("Ok"), action: {
+                        // set isUserAuthenticated to signed out
+            }))
         }
     }
+    
+    private func performExistingAccountSetupFlows() {
+        
+    }
+    
+    private func performSignIn(using request: [ASAuthorizationRequest]) {
+        
+        signInWithappleDelegates = SignInWithAppleDelegates
+        
+        let controller = ASAuthorizationController(authorizationRequests: requests)
+        controller.delegate = SignInWithAppleDelegates
+        controller.presentationContextProvider = SignInWithAppleDelegates
+        
+        controller.performRequests()
+    }
 }
+    
 
 struct LaunchScreenView_Previews: PreviewProvider {
     static var previews: some View {
